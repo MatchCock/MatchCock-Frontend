654bb9c5e5d4a36c5944646ecf01d331753906b5
ashveil-dev <ashveil-dev@github.com>
2025-11-03 15:30:14 +0900

    @Design 플로팅 메뉴 만들기

---
+++ src/common/FloatMenu.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+function FloatMenu() {
+    const [mobileMenuButtonClicked, setMobileMenuButtonClicked] = useState(false);
+
+    const onMobileMenuButtonClick = () => setMobileMenuButtonClicked(b => !b);
+    
+    return (
+        <nav className="h-full fixed right-5 bottom-5 flex flex-col-reverse justify-start gap-6">
+            <div
+                onClick={onMobileMenuButtonClick}
+                className="w-20 h-20 box-content p-2 aspect-square flex justify-center items-center rounded-2xl shadow-2xl bg-RoyalAmethyst text-white text-2xl font-bold font-Pretendard cursor-pointer">
+                메뉴
+            </div>
+            <div
+                id="menuWrap"
+                style={{ scale: mobileMenuButtonClicked ? "1" : "0" }}
+                className="transition-transform origin-bottom duration-150 shadow-2xl rounded-2xl flex flex-col gap-5 py-3 px-2"
+            >
+                <div
+                    id="tournament"
+                    tabIndex={0}
+                    // style={{"backgroundPosition" : "-3px -203px"}}
+                    className="w-20 h-20 shrink-0 
+                                bg-size-[300px] bg-position-[-3px_-23px] hover:bg-position-[-3px_-108px] focus:bg-position-[-3px_-108px] 
+                                bg-[url('assets/images/MenuSprite.png')] bg-no-repeat cursor-pointer"
+                />
+
+                <div
+                    id="club"
+                    tabIndex={0}
+                    // style={{"backgroundPosition" : "-75px -203px"}}
+                    className="w-20 h-20 shrink-0
+                            bg-size-[300px] 
+                                bg-position-[-75px_-23px] hover:bg-position-[-75px_-108px] focus:bg-position-[-75px_-108px] 
+                                bg-[url('assets/images/MenuSprite.png')] bg-no-repeat cursor-pointer"
+                />
+                <div
+                    id="team"
+                    tabIndex={0}
+                    // style={{"backgroundPosition" : "-148px -203px"}}
+                    className="w-20 h-20 shrink-0
+                            bg-size-[300px] 
+                                bg-position-[-148px_-23px] hover:bg-position-[-148px_-108px] focus:bg-position-[-148px_-108px] 
+                                bg-[url('assets/images/MenuSprite.png')] bg-no-repeat cursor-pointer"
+                />
+                <div
+                    id="schedule"
+                    tabIndex={0}
+                    // style={{"backgroundPosition" : "-221px -203px"}}
+                    className="w-20 h-20 shrink-0
+                            bg-size-[300px] 
+                                        bg-position-[-221px_-23px] hover:bg-position-[-221px_-108px] focus:bg-position-[-221px_-108px] 
+                                bg-[url('assets/images/MenuSprite.png')] bg-no-repeat cursor-pointer"
+                />
+            </div>
+
+        </nav>
+    )
+}
+
+export default FloatMenu;
