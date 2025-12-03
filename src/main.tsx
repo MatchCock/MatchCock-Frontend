1e60eb9f04aa3b8a9bfd85bb8bc0995f04935fe4
ashveil-dev <ashveil.dev@gmail.com>
2025-12-03 14:46:29 +0900

    @Setting react-router 패키지 추가하기

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import "./index.css"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router"
+import App from "App";
 import Tournament from '@pages/Tournament/index'
 
 const queryClient = new QueryClient();
@@ -9,7 +11,12 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Tournament />
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/MatchCock" element={<App />} />
+          <Route path="/MatchCock/tournament" element={<Tournament />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 )
